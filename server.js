a811736dfe298dc336c6d824dbd0224ee659de60
cgkaczenski <kzpulaski@gmail.com>
2016-04-04 14:33:30 -0500

    simple user functionality

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require('express'),
 	app = express(),
 	server = require('http').createServer(app),
-	io = require('socket.io').listen(server);
+	io = require('socket.io').listen(server),
+	usernames = [];
 
 server.listen(process.env.PORT || 3000, function() {
 	console.log("server running");
@@ -14,7 +15,32 @@ app.get('/', function(req, res){
 io.sockets.on('connection', function(socket) {
 	console.log('socket connected');
 
+	socket.on('new user', function(data, callback){
+		//check for duplicate user
+		if(usernames.indexOf(data) != -1){
+			callback(false);
+		}else{
+			callback(true);
+			socket.username = data;
+			usernames.push(socket.username);
+			updateUsernames();
+		}
+	});
+
+	function updateUsernames(){
+		io.sockets.emit('usernames', usernames);
+	}
+
 	socket.on('send message', function(data){
-		io.sockets.emit('new message', {msg: data});
+		io.sockets.emit('new message', {msg: data, user: socket.username});
+	});
+
+	socket.on('disconnect', function(data){
+		if(!socket.username){
+			return;
+		}
+
+		usernames.splice(usernames.indexOf(socket.username), 1);
+		updateUsernames();
 	});
 });
